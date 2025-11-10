d8b4f0ef4302965e4afb02b22d5d625e28b282da
“sgmunoz1” <sgmunoz@espol.edu.ec>
2025-11-10 16:33:01 -0500

    Agregar paser.py

---
+++ yacc/parser.py
@@ -0,0 +1,101 @@
+import ply.yacc as yacc
+from lexer.lexer import tokens
+
+
+def p_asignacion(p):
+    """asignacion : IDENTIFIER ASSIGN valor"""
+
+
+def p_valor(p):
+    """valor : IDENTIFIER
+    | expresion
+    | STRING
+    | bool"""
+
+
+def p_expresion(p):
+    """expresion : expresionMatematica
+    """
+
+
+def p_boolean(p):
+    """bool : TRUE
+    | FALSE"""
+
+
+def p_expresionMatematica(p):
+    """expresionMatematica : numero operando expresionMatematica
+    | numero"""
+
+
+def p_operando(p):
+    """operando : PLUS
+    | MINUS
+    | TIMES
+    | DIVIDE
+    | MODULO"""
+
+
+def p_numero(p):
+    """numero : INTEGER
+    | FLOAT"""
+
+
+""" def p_expression_plus(p):
+    "expression : expression PLUS term"
+    p[0] = p[1] + p[3]
+
+
+def p_expression_minus(p):
+    "expression : expression MINUS term"
+    p[0] = p[1] - p[3]
+
+
+def p_expression_term(p):
+    "expression : term"
+    p[0] = p[1]
+
+
+def p_term_times(p):
+    "term : term TIMES factor"
+    p[0] = p[1] * p[3]
+
+
+def p_term_div(p):
+    "term : term DIVIDE factor"
+    p[0] = p[1] / p[3]
+
+
+def p_term_factor(p):
+    "term : factor"
+    p[0] = p[1]
+
+
+def p_factor_num(p):
+    "factor : NUMBER"
+    p[0] = p[1]
+
+
+def p_factor_expr(p):
+    "factor : LPAREN expression RPAREN"
+    p[0] = p[2]
+ """
+
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input("calc > ")
+    except EOFError:
+        break
+    if not s:
+        continue
+    result = parser.parse(s)
+    print(result)
